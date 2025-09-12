2cd4a492f4f5135549a5f0a73775ad37bf446284
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-09-12 13:51:40 +0100

    Add Button component with two variants

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,32 @@
+import { cn } from '@/lib/utils';
+import { ButtonHTMLAttributes, ReactElement, forwardRef } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  children?: ReactElement | ReactElement[] | string;
+  variant?: 'default' | 'outline';
+}
+
+const Button = forwardRef<HTMLButtonElement, Props>(
+  ({ className, children, variant = 'default', ...props }, ref) => {
+    return (
+      <button
+        {...props}
+        ref={ref}
+        className={cn(
+          'inline-flex cursor-pointer items-center justify-center px-12 py-4 text-center font-extrabold tracking-wider uppercase transition-colors duration-300',
+          variant === 'default' && 'bg-white text-black hover:bg-white/80',
+          variant === 'outline' &&
+            'border border-white text-white hover:bg-white/20',
+          className,
+        )}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+Button.displayName = 'Button';
+
+export default Button;
