3f0294cbab07cf5597108e10abdf9ce4f88084ba
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-09-14 13:08:50 +0100

    Add ScrollText section with GSAP animations and integrate into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/sections/Hero';
+import ScrollText from '@/components/sections/ScrollText';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <ScrollText />
+    </>
+  );
 }
