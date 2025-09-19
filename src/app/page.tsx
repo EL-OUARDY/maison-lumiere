e2e7fafac29b4f2e10ca1eb260c42d7e3123f97f
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-09-19 13:48:07 +0100

    Add FeaturesGrid section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/sections/Hero';
 import ScrollText from '@/components/sections/ScrollText';
+import FeaturesGrid from '@/components/sections/FeaturesGrid';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ScrollText />
+      <FeaturesGrid />
       <section className="min-h-screen bg-emerald-500"></section>
       <section className="min-h-screen bg-rose-500"></section>
       <section className="min-h-screen bg-purple-500"></section>
---
+++ src/components/sections/FeaturesGrid.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Bounded } from '@/components/shared/Bounded';
+import Image from 'next/image';
+import Button from '@/components/ui/button';
+
+function FeaturesGrid() {
+  return (
+    <Bounded className="flex items-center justify-center overflow-hidden bg-neutral-950 py-16 text-center md:py-24">
+      <div className="flex flex-col gap-8 lg:flex-row">
+        <div className="grid-image relative hidden w-full lg:block lg:flex-2">
+          <Image
+            className="object-cover"
+            src="/img/grid-picture.jpg"
+            alt=""
+            fill
+            sizes="100vw"
+          />
+        </div>
+
+        <div className="description w-full space-y-12 lg:flex-1">
+          <div className="space-y-6 bg-white/10 p-10 text-left">
+            <h2 className="text-3xl leading-tight font-semibold md:text-4xl">
+              Effortless Distinction.
+            </h2>
+            <p className="max-w-lg text-base text-gray-300">
+              True elegance requires no announcement. Maison Lumière embodies
+              this philosophy—a fragrance for those who command respect through
+              presence alone. Contemporary luxury rooted in enduring principles.
+            </p>
+          </div>
+
+          <div className="product-card space-y-6 bg-white/10 p-10 text-left">
+            <div className="image-bottle relative -mt-28 aspect-[3/3.5] -rotate-12">
+              <Image
+                className="object-cover"
+                src="/img/ignis-bottle.png"
+                alt=""
+                fill
+                sizes="100vw"
+              />
+            </div>
+            <div className="flex justify-between">
+              <div className="space-y-2">
+                <h2 className="font-title text-4xl">Ignis</h2>
+                <span className="text-gray-400">Eau de Parfum</span>
+              </div>
+              <div className="price mt-4 text-gray-100">$130</div>
+            </div>
+
+            <Button variant="outline">Shop Now</Button>
+          </div>
+        </div>
+      </div>
+    </Bounded>
+  );
+}
+
+export default FeaturesGrid;
