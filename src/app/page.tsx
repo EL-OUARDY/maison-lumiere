85b4b3d12ec7ef5689a2fc57d4a575984effa026
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-09-20 13:21:55 +0100

    Add Video component and integrate into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import ScrollText from '@/components/sections/ScrollText';
 import FeaturesGrid from '@/components/sections/FeaturesGrid';
 import FragranceList from '@/components/sections/Fragrance/FragranceList';
 import CallToAction from '@/components/sections/CallToAction';
+import Video from '@/components/sections/Video';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <FeaturesGrid />
       <FragranceList />
       <CallToAction />
+      <Video />
     </>
   );
 }
