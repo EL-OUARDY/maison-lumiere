b6d30fa55a6945dab254c8f67ef93bc5a8af9e81
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-09-21 22:47:27 +0100

    FIX: Force scroll top with lenis when pathname changes

--- src/components/shared/LenisScrollProvider.tsx
+++ src/components/shared/LenisScrollProvider.tsx
@@ -2,6 +2,7 @@
 import ReactLenis, { LenisRef } from 'lenis/react';
 import React, { ReactNode, useEffect, useRef } from 'react';
 import gsap from 'gsap';
+import { usePathname } from 'next/navigation';
 
 interface Props {
   children: ReactNode;
@@ -9,7 +10,9 @@ interface Props {
 
 function LenisScrollProvider({ children }: Props) {
   const lenisRef = useRef<LenisRef | null>(null);
+  const pathname = usePathname();
 
+  // GSAP integration
   useEffect(() => {
     function update(time: number) {
       lenisRef.current?.lenis?.raf(time * 1000);
@@ -19,6 +22,16 @@ function LenisScrollProvider({ children }: Props) {
 
     return () => gsap.ticker.remove(update);
   }, []);
+
+  // Force scroll top
+  useEffect(() => {
+    const lenisInstance = lenisRef.current?.lenis;
+    if (lenisInstance) {
+      lenisInstance.scrollTo(0, { immediate: true });
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]);
 
   return (
     <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
