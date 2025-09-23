7c7f1075be15c4b1656a75507e336b8cd1c2049a
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-09-23 19:45:05 +0100

    Cart functionality

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -20,7 +20,7 @@ interface Props {
 }
 
 function Cart({ onClose }: Props) {
-  const { cart, addToCart, removeFromCart, clearCart } = useStore();
+  const { cart, addToCart, removeFromCart } = useStore();
 
   return (
     <div className="cart flex size-full flex-col" aria-label="cart">
@@ -33,20 +33,21 @@ function Cart({ onClose }: Props) {
       </div>
 
       <div className="cart-body mx-auto flex w-full max-w-2xl flex-1 items-center justify-center">
-        {cart ? (
+        {cart.length > 0 ? (
           <div className="flex w-full flex-col items-center justify-center gap-4">
             <RevealText
               as={'h3'}
               delay={0.4}
-              className="font-title w-full border-b border-neutral-700 pb-2 text-4xl capitalize"
-              text={`Cart (${cart.length})`}
+              className="font-title w-full flex-1 border-b border-neutral-700 text-4xl capitalize"
+              text={`Cart`}
               hoverLine={false}
             />
 
             <div className="cart-items flex w-full flex-col gap-4">
               {cart.map((item, index) => (
-                <div
-                  className="cart-item flex h-24 w-full items-center justify-center gap-6 border-l-2 border-white bg-gradient-to-r from-neutral-900 to-neutral-800 px-2 py-4"
+                <FadeIn
+                  vars={{ delay: index * 0.1 + 0.3, duration: 1.3 }}
+                  className="cart-item flex h-24 w-full translate-y-16 items-center justify-center gap-6 border-l-2 border-white bg-gradient-to-r from-neutral-900 to-neutral-800 px-2 py-4"
                   key={index}
                 >
                   <div className="cart-item-image size-16 translate-x-2 rotate-7">
@@ -69,37 +70,46 @@ function Cart({ onClose }: Props) {
                     </div>
 
                     <div className="flex">
-                      <div className="cart-item-features flex flex-col gap-1 text-sm text-gray-300">
+                      <div className="cart-item-features flex items-center justify-center gap-2 text-gray-300">
                         {item.features.map((feature, index) => (
                           <p
                             key={index}
-                            className="flex items-center gap-2 text-[0.7rem] hover:text-white"
+                            className="flex cursor-pointer items-center gap-1 text-[0.8rem] hover:text-white"
                           >
-                            <feature.icon className="size-3" />
+                            <feature.icon className="size-3.5" />
                             {feature.label}
                           </p>
                         ))}
                       </div>
                       <div className="cart-item-quantity flex flex-1 items-end justify-end gap-2">
-                        <button className="flex size-8 cursor-pointer items-center justify-center rounded-md border border-white/10 text-neutral-400 transition duration-300 hover:bg-white/10 hover:text-white">
+                        <button
+                          onClick={() => removeFromCart(item, false)}
+                          className="flex size-7 cursor-pointer items-center justify-center rounded-md border border-white/10 text-neutral-400 transition duration-300 hover:bg-white/10 hover:text-white"
+                        >
                           <MinusIcon className="size-3" />
                         </button>
-                        <div className="flex size-8 items-center justify-center rounded-md border border-white/10 text-sm">
+                        <div className="flex size-7 items-center justify-center rounded-md border border-white/10 text-sm">
                           {item.quantity}
                         </div>
-                        <button className="flex size-8 cursor-pointer items-center justify-center rounded-md border border-white/10 text-neutral-400 transition duration-300 hover:bg-white/10 hover:text-white">
+                        <button
+                          onClick={() => addToCart(item)}
+                          className="flex size-7 cursor-pointer items-center justify-center rounded-md border border-white/10 text-neutral-400 transition duration-300 hover:bg-white/10 hover:text-white"
+                        >
                           <PlusIcon className="size-3" />
                         </button>
                       </div>
                     </div>
                   </div>
-                  <div className="cart-item-price font-title px-2 text-2xl">
+                  <div className="cart-item-price font-title text-2xl">
                     ${item.price}
                   </div>
-                  <button className="cart-item-controls cursor-pointer p-2 text-neutral-400 transition duration-300 hover:bg-white/10 hover:text-white">
+                  <button
+                    onClick={() => removeFromCart(item, true)}
+                    className="cart-item-controls cursor-pointer p-2 text-neutral-400 transition duration-300 hover:bg-white/10 hover:text-white"
+                  >
                     <XIcon className="size-5" />
                   </button>
-                </div>
+                </FadeIn>
               ))}
             </div>
 
@@ -116,9 +126,16 @@ function Cart({ onClose }: Props) {
                 className="translate-y-8"
                 vars={{ delay: 0.7, duration: 1.3 }}
               >
-                <Button variant="default" className="!py-2 text-sm">
+                <Button variant="default" className="!px-8 !py-2 text-sm">
                   <ShoppingBagIcon className="mr-2 size-4 opacity-70" />{' '}
-                  <span>Checkout</span>
+                  <span>
+                    Checkout (
+                    {cart.reduce(
+                      (accumulator, current) => accumulator + current.quantity,
+                      0,
+                    )}
+                    )
+                  </span>
                 </Button>
               </FadeIn>
             </div>
